b1142cb3798a252f15b4a9c7254fda579e346bdc
Bahtiar Ariyanki <bahtiarariyanki@Bahtiars-MacBook-Air.local>
2023-12-11 14:00:30 +0700

    init

--- .github/actions/md-to-pdf/entrypoint.sh
+++ .github/actions/md-to-pdf/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_FOLDER=$1
-OUTPUT_FOLDER=$2
+INPUT_FOLDER=$INPUT_MD_FOLDER
+OUTPUT_FOLDER=$INPUT_PDF_FOLDER
 
 find "$INPUT_FOLDER" -name '*.md' -exec pandoc -s {} -o "$OUTPUT_FOLDER/{}.pdf" \;
