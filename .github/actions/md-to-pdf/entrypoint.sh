7172ce7c8109d166439838ba71e2abd629822b8d
Bahtiar Ariyanki <bahtiarariyanki@Bahtiars-MacBook-Air.local>
2023-12-12 15:36:09 +0700

    init

--- .github/actions/md-to-pdf/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir $INPUT_PDF_FOLDER_NAME
-
-while IFS= read -r md_file; do
-    echo "converting $md_file"
-    pandoc "$md_file" -s -o "$INPUT_PDF_FOLDER_NAME/${md_file%.md}.pdf"
-done < $INPUT_LIST_MD_FILES
